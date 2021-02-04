0d41cf5704bb483c5cb0c7517ac40bfa95a8ed6c
bradenc98 <78054354+bradenc98@users.noreply.github.com>
2021-02-04 14:24:19 -0700

    Add files via upload
    first time uploading files

---
+++ c_variables.cpp
@@ -0,0 +1,24 @@
+#I made this comment at 2:08
+# include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+int a,b;
+
+int c;
+
+a=5;
+b=3;
+
+c=a^b;
+
+cout<< result;
+cout<< "hello world";
+
+return 0;
+
+}
+
